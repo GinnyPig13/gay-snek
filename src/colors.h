fec33c9a9726651470f22fb4375218e323c9929a
SOLACE\conankzhang <conankzhang@gmail.com>
2021-12-28 22:03:19 -0800

    colors: Make const

--- src/colors.h
+++ src/colors.h
@@ -4,14 +4,14 @@
 
 namespace KCC::colors
 {
-    static ofColor babyPowderWhite(255, 255, 252);
-    static ofColor lightPinkRed(255, 173, 173);
-    static ofColor deepChampagneOrange(255, 214, 165);
-    static ofColor lemonCremeYellow(253, 255, 182);
-    static ofColor teaGreen(202, 255, 191);
-    static ofColor celesteLightBlue(155, 246, 255);
-    static ofColor babyEyesBlue(160, 196, 255);
-    static ofColor maximumBluePurple(189, 178, 255);
-    static ofColor mauvePink(255, 198, 255);
-    static ofColor richBlack(1, 22, 39);
+    static const ofColor babyPowderWhite(255, 255, 252);
+    static const ofColor lightPinkRed(255, 173, 173);
+    static const ofColor deepChampagneOrange(255, 214, 165);
+    static const ofColor lemonCremeYellow(253, 255, 182);
+    static const ofColor teaGreen(202, 255, 191);
+    static const ofColor celesteLightBlue(155, 246, 255);
+    static const ofColor babyEyesBlue(160, 196, 255);
+    static const ofColor maximumBluePurple(189, 178, 255);
+    static const ofColor mauvePink(255, 198, 255);
+    static const ofColor richBlack(1, 22, 39);
 }
